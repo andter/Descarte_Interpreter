4a4f1f930eb5ac6c072dcc8a03c7a4cc2ed534e4
andy <andrewjoelbecker@gmail.com>
2015-11-23 21:59:34 -0700

    If Else working

--- src/elsePart.java
+++ src/elsePart.java
@@ -2,4 +2,8 @@
  * Created by andy on 11/23/2015.
  */
 public class elsePart {
+    public elsePart(TokenList tokens){
+        System.out.println("ELSEPART-----");
+        tokens.printList();
+    }
 }
--- src/ifStmt.java
+++ src/ifStmt.java
@@ -16,13 +16,18 @@ public class ifStmt implements Expression{
             int beginFI = tokens.indexOf("FI");
             if(beginThen != -1) {
                 if (beginElse == -1) { //No else statement
-                    if (beginFI != -1 && beginThen < beginFI) {
+                    if (beginFI != -1 && (beginThen < beginFI)) {
                         expression = new expr(tokens.between(1, beginThen));
                         statementList = new stmtList(tokens.between(beginThen+1, beginFI));
+                        elsePart = new elsePart(tokens.between(beginFI, beginFI+1));
                     }
                 }
                 else{
-
+                    if(beginFI != -1 && (beginThen < beginElse) && (beginElse < beginFI)){
+                        expression = new expr(tokens.between(1, beginThen));
+                        statementList = new stmtList(tokens.between(beginThen+1, beginElse));
+                        elsePart = new elsePart(tokens.between(beginElse, beginFI+1));
+                    }
                 }
             }
         }
